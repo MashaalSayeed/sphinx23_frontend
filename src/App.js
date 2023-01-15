ea0d2ce0197a9c1d4efc6a735af06cef06f6d40f
Rupesh Yadav <rupeshyadav8178@gmail.com>
2023-01-15 10:10:35 +0530

    login comp

--- src/App.js
+++ src/App.js
@@ -25,14 +25,8 @@ import PassDetail from "./screens/containers/Dashboard/SuperAdmin/Pass/PassDetai
 import CreatePass from "./screens/containers/Dashboard/SuperAdmin/Pass/createPass";
 import EventDetails from "./screens/components/EventDetails/EventDetails";
 import DashboardEventAdmin from "./screens/containers/Dashboard/EventAdmin/DashboardEventAdmin";
-import DashboardRegular from "./screens/containers/Dashboard/DashboardRegular";
-import LoginScreen from "./screens/containers/Login/Login/LoginScreen";
-import CreateAccScreen from "./screens/containers/Login/CreateAccount/createAccScreen";
-import OtpScreen from "./screens/containers/Login/Login/OTP.js/OtpScreen";
-import AddDetailscreen from "./screens/containers/Login/AddDetails/AddDetailscreen";
-import ForgotPass from "./screens/containers/Login/Login/ForgotPass";
-
-import Complaints from "./screens/containers/Dashboard/EventAdmin/Complaints/Complaints";
+import LoginScreen from "./screens/containers/Login/LoginScreen";
+import Landing from "./screens/containers/Home/home";
 
 function App() {
   // const token1 =
@@ -75,15 +69,9 @@ function App() {
         {/* <Navbar /> */}
         {curruser != null ? (
           <Routes>
-            <Route exact path="/" element={<DashboardRegular />} />
-            <Route path="/sign-in" element={<LoginScreen />} />
-            <Route path="/sign-up" element={<CreateAccScreen />} />
-            <Route path="/otp" element={<OtpScreen />} />
-            <Route path="/addDetails" element={<AddDetailscreen />} />
-            {/* <Route exact path="/" element={<DashboardSuperAdmin />} /> */}
+            <Route exact path="/" element={<LoginScreen />} />
             <Route path="/eventAdmin" element={<DashboardEventAdmin />} />
             <Route path="/superAdmin" element={<DashboardSuperAdmin />} />
-            <Route path="/forgotpass" element={<ForgotPass />} />
             <Route path="/superAdmin/pass/:id" element={<PassDetail />}></Route>
             <Route
               path="/eventDetails/event/:id"
--- src/screens/containers/Home/home.js
+++ src/screens/containers/Home/home.js
@@ -1,13 +1,57 @@
-import React from "react";
-import "../../../styles/home.css";
-import grad from "../../../images/home/gradBack.png";
+// import React, { useState } from "react";
+// import "../../../styles/home.css";
+// import grad from "../../../images/home/gradBack.png";
+// import stars from "../../../images/home/starsDim.png";
+// import pyraminds from "../../../images/home/homePyramid1.png";
+// import logo from "../../../images/home/homeLogo.png";
+// import burger from "../../../images/home/hamburger.png";
 
-function Landing() {
-  return (
-    <div className="landing-main">
-      <img className="landing-gradient" src={grad}></img>
-    </div>
-  );
-}
+// function Landing() {
+//   const [currTab, setCurrTab] = useState("Home");
+//   const Tabs = ["Home", "About", "Contact"];
+//   const Navbar = () => {
+//     return (
+//       <div className="landing-navbar">
+//         <div className="landing-logo">
+//           <img src={logo}></img>
+//         </div>
+//         <div className="landing-tabs">
+//           {Tabs.map((value, i) => {
+//             return (
+//               <div
+//                 className="landing-tab-link"
+//                 key={i}
+//                 onClick={() => {
+//                   console.log(value);
+//                   setCurrTab(value);
+//                 }}
+//                 style={
+//                   currTab == value ? { textShadow: "0px 1px 3px #FFFFFF " } : {}
+//                 }
+//               >
+//                 {value}
+//               </div>
+//             );
+//           })}
+//         </div>
+//         <div className="landing-ham">
+//           <img src={burger}></img>
+//         </div>
+//       </div>
+//     );
+//   };
+//   return (
+//     <div className="landing-main">
+//       <img className="landing-gradient" src={grad}></img>
+//       <img className="landing-stars" src={stars}></img>
+//       {Navbar()}
+//       <div className="landing-title">SPHINX</div>
+//       <div className="landing-pyramids">
+//         {" "}
+//         <img src={pyraminds}></img>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Landing;
+// export default Landing;
