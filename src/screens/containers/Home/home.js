4aca1a91e88f9e6ff4063eb7c23ac9c37479fdab
Rupesh Yadav <rupeshyadav8178@gmail.com>
2023-01-17 23:06:57 +0530

    landing comp

--- src/screens/containers/Home/home.js
+++ src/screens/containers/Home/home.js
@@ -22,37 +22,122 @@ function Landing() {
     return () => clearTimeout(timeout);
   }, []);
 
+  const exitStyle = {
+    animation: "exitBright 2000ms ease-in-out",
+    animationFillMode: "forwards",
+  };
+
   return (
     <>
-      <div
-        className={Loading || !Scroll ? "landing-main" : "exit-anim"}
-        onWheel={() => {
-          if (!Loading) {
-            setScroll(true);
-          }
-        }}
-      >
-        <img className="landing-gradient" src={grad}></img>
-        <div className="stars-con">
-          <img className="landing-stars" src={stars}></img>
-        </div>
-        <HomeNav setCurrTab={setCurrTab} currTab={currTab} Tabs={Tabs} />
-        <div className="landing-title">SPHINX</div>
-        <div className="landing-pyramids">
-          {" "}
-          {Loading || !Scroll ? (
-            <div className="landing-line-img"></div>
-          ) : (
-            <></>
-          )}
-          {/* <div className="landing-line-img"></div> */}
-          <img className="landing-pyramids-img" src={pyraminds}></img>
-        </div>
-        <div className="landing-scroll">
-          <div className="landing-scroll-text">SCROLL</div>
-          <div className="landing-scroll-line"></div>
+      <div>
+        <div
+          className={Loading || !Scroll ? "landing-main" : "exit-anim"}
+          onWheel={() => {
+            if (!Loading) {
+              setScroll(true);
+            }
+          }}
+        >
+          <img
+            className="landing-gradient"
+            src={grad}
+            style={
+              Loading || !Scroll
+                ? {}
+                : {
+                    animation: "exitBright 2000ms ease-in-out",
+                    animationFillMode: "forwards",
+                  }
+            }
+          ></img>
+          <div
+            className="stars-con"
+            style={Loading || !Scroll ? {} : exitStyle}
+          >
+            <img className="landing-stars" src={stars}></img>
+          </div>
+          <div
+            style={
+              Loading || !Scroll
+                ? {}
+                : {
+                    animation: "exitHide 2000ms ease-in-out",
+                    animationFillMode: "forwards",
+                  }
+            }
+            className="newNavbar"
+          >
+            <HomeNav setCurrTab={setCurrTab} currTab={currTab} Tabs={Tabs} />
+          </div>
+
+          <div
+            className={
+              Loading || !Scroll ? "landing-title" : "landing-titleanim"
+            }
+            style={
+              Loading || !Scroll
+                ? {}
+                : {
+                    animation: "titleAnim 2000ms ease-in-out",
+                    animationFillMode: "forwards",
+                  }
+            }
+          >
+            SPHINX
+          </div>
+          <div className="landing-pyramids">
+            {" "}
+            {Loading || !Scroll ? (
+              <div
+                className="landing-line-img"
+                style={
+                  Loading || !Scroll
+                    ? {}
+                    : {
+                        animation: "exitBright 2000ms ease-in-out",
+                        animationFillMode: "forwards",
+                      }
+                }
+              ></div>
+            ) : (
+              <></>
+            )}
+            {/* <div className="landing-line-img"></div> */}
+            <img
+              className="landing-pyramids-img"
+              src={pyraminds}
+              style={
+                Loading || !Scroll
+                  ? {}
+                  : {
+                      animation: "exitBright 2000ms ease-in-out",
+                      animationFillMode: "forwards",
+                    }
+              }
+            ></img>
+          </div>
+          <div
+            className="landing-scroll"
+            style={
+              Loading || !Scroll
+                ? {}
+                : {
+                    animation: "exitHide 2000ms ease-in-out",
+                    animationFillMode: "forwards",
+                  }
+            }
+          >
+            <div className="landing-scroll-text">SCROLL</div>
+            <div className="landing-scroll-line"></div>
+          </div>
         </div>
       </div>
+      {/* <div
+        className={Loading || !Scroll ? "landing-title" : "landing-titleanim"}
+      >
+        SPHINX
+      </div> */}
+
       {Loading || !Scroll ? (
         <></>
       ) : (
