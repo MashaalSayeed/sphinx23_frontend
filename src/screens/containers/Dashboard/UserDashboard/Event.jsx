4d92fb86d04727e76ddc2bb5e2843f236a2e20e6
Logicless-Coder <abhirathore565@gmail.com>
2023-02-03 14:42:37 +0530

    Created a dialog showing team details for every event

--- src/screens/containers/Dashboard/UserDashboard/Event.jsx
+++ src/screens/containers/Dashboard/UserDashboard/Event.jsx
@@ -1,48 +1,68 @@
 import "./../../../../styles/userDashboard.css";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
 
 const Event = ({ data }) => {
-  console.log(data);
-  let date = new Date(data.from);
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  return (
-    <div className="ud__event">
-      <img
-        className="ud__event__thumbnail"
-        src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.color-hex.com%2Fpalettes%2F4666.png&sp=1675173524T36d7c9eb670f126ff0d0c62c231f8fd65a4a0f13131f2849c9c1ff52c5cee3d2"
-        alt="event thumbnail"
-      />
-      <div className="ud__event__details">
-        <p className="ud__event__badge">{data.category}</p>
-        <div className="ud__event__title-subtitle">
-          <h3 className="ud__event__title">{data.name}</h3>
-          {/* <h4 className="ud__event__subtitle">By ZINE</h4> */}
-        </div>
-        <p className="ud_event_date">
-          {" "}
-          {date.getDate() +
-            " " +
-            monthNames[date.getUTCMonth()] +
-            "," +
-            date.getFullYear()}
-        </p>
-        <p className="ud_event_date"> {data.time}</p>
-      </div>
-      <button className="ud__event__team-details-button">Team Details</button>
-    </div>
-  );
+	const [dialogOpen, setDialogOpen] = useState(false);
+
+	const openDialog = () => setDialogOpen(true);
+	const closeDialog = () => setDialogOpen(false);
+
+	let date = new Date(data.from);
+	const monthNames = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	return (
+		<div className="ud__event">
+			<img
+				className="ud__event__thumbnail"
+				src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.color-hex.com%2Fpalettes%2F4666.png&sp=1675173524T36d7c9eb670f126ff0d0c62c231f8fd65a4a0f13131f2849c9c1ff52c5cee3d2"
+				alt="event thumbnail"
+			/>
+			<div className="ud__event__details">
+				<p className="ud__event__badge">{data.category}</p>
+				<div className="ud__event__title-subtitle">
+					<h3 className="ud__event__title">{data.name}</h3>
+					<h4 className="ud__event__subtitle">By ZINE</h4>
+				</div>
+				<p className="ud_event_date">
+					{" "}
+					{date.getDate() +
+						" " +
+						monthNames[date.getUTCMonth()] +
+						"," +
+						date.getFullYear()}
+				</p>
+				<p className="ud_event_date"> {data.time}</p>
+			</div>
+			<button className="ud__event__team-details-button" onClick={openDialog}>
+				Team Details
+			</button>
+			\
+			<Dialog open={dialogOpen} onClose={closeDialog}>
+				<div className="ud__event__team-details-content">
+					<h4 className="ud__event__team-details-title">Team Name</h4>
+					<ul className="ud__event__team-details-list">
+						<li>Team Member 1</li>
+						<li>Team Member 2</li>
+						<li>Team Member 3</li>
+					</ul>
+				</div>
+			</Dialog>
+		</div>
+	);
 };
 
 export default Event;
--- src/screens/containers/Dashboard/UserDashboard/Profile.jsx
+++ src/screens/containers/Dashboard/UserDashboard/Profile.jsx
@@ -2,20 +2,20 @@ import "./../../../../styles/userDashboard.css";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const currUser = useSelector((state) => state.auth.curruser);
-  return (
-    <div className="ud__profile">
-      <img
-        className="ud__profile__image"
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-        alt="profile"
-      />
-      <div className="ud__profile__name">
-        <h1 className="ud__profile__name_h1">{currUser.profile.name}</h1>
-        <h3 className="ud__profile__name_h3">{currUser.profile.collegeName}</h3>
-      </div>
-    </div>
-  );
+	const currUser = useSelector((state) => state.auth.curruser);
+	return (
+		<div className="ud__profile">
+			<img
+				className="ud__profile__image"
+				src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+				alt="profile"
+			/>
+			<div className="ud__profile__name">
+				<h1 className="ud__profile__name_h1">{currUser.profile.name}</h1>
+				<h3 className="ud__profile__name_h3">{currUser.profile.collegeName}</h3>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
