7f2c54eee6627d4a22ffa869721d76a2dac43d3e
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2023-02-06 16:52:08 +0530

    Minor Change

---
+++ src/screens/containers/OurTeam/advisors.js
@@ -0,0 +1,43 @@
+let advisors = [
+  {
+    name: "Avnish Yash",
+    position: "Advisor to Vice-President",
+    phone: 9472287823,
+    email: "2019uce1693@mnit.ac.in",
+    linkedUrl: "https://www.linkedin.com/in/avnish-yash-8809261a9",
+    imageUrl: "AvnishYash",
+  },
+  {
+    name: "Naman Jain",
+    position: "Advisor to Technical Secretary",
+    phone: 9461818657,
+    email: "2019ucp1390@mnit.ac.in",
+    linkedUrl: "https://www.linkedin.com/in/naman-jain-1b4131201/",
+    imageUrl: "NamanJain",
+  },
+  {
+    name: "Sidhanto Sujoy Mukerji",
+    position: "Advisor to Financial Secretary",
+    phone: 9825662578,
+    email: "2019uce1175@mnit.ac.in",
+    linkedUrl: "https://www.linkedin.com/in/sidhantomukerji",
+    imageUrl: "SidhantoSujoyMukerji",
+  },
+  {
+    name: "Hemant Chaudhary",
+    position: "Advisor to Marketing and Sponsorship Secretary",
+    phone: 9081005964,
+    email: "hemantchaudhary239@gmail.com",
+    linkedUrl: "https://www.linkedin.com/in/hemant-chaudhary-854853200",
+    imageUrl: "HemantChaudhary",
+  },
+  {
+    name: "Karan Jangu",
+    position: "Advisor to Logistics Secretary",
+    phone: 9799574566,
+    email: "karanjangu2001@gmail.com",
+    linkedUrl: " https://www.linkedin.com/in/karan-61b050196",
+    imageUrl: "KaranJangu",
+  },
+];
+export default advisors;
