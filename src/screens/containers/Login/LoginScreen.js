d9ecb749f9f0177fb271dd73da7b5f3d80ca1eac
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2023-02-18 10:30:33 +0530

    Merge

--- src/screens/containers/Login/LoginScreen.js
+++ src/screens/containers/Login/LoginScreen.js
@@ -284,11 +284,11 @@ function Login(props) {
             Login
           </button>
           <Seprator />
-          <SocialIcons
+          {/* <SocialIcons
             handleSuccess={handleSuccess}
             handleFailure={handleFailure}
             isRegistration={false}
-          />
+          /> */}
         </>
       )}
       {forgot && <ForgotPass />}
@@ -498,11 +498,11 @@ function RegScreen1(props) {
   return (
     <>
       <div className="login-form-title">Create an Account</div>
-      <SocialIcons
+      {/* <SocialIcons
         handleSuccess={handleSuccess}
         handleFailure={handleFailure}
         isRegistration={true}
-      />
+      /> */}
       <Seprator />
       <div className="login-form-sub-title">Sign-up with email</div>
       <div className="login-form-signup">
