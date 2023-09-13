eb9fb4257fc05fd98d20ed52890dc11e00a5711d
herschelleT <herschellethomas10@gmail.com>
2023-09-13 21:05:39 +0530

    profile0.2

--- src/screens/containers/Dashboard/UserDashboard/Profile.jsx
+++ src/screens/containers/Dashboard/UserDashboard/Profile.jsx
@@ -1,59 +1,109 @@
-import "./../../../../styles/profile.css";
+import "../../../../styles/profile.css";
 import prof from "../../../../images/user.png";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import EventCard from "../../Events/EventCard.js"
+import React, { useState, useEffect } from "react";
+import EventCard from "../../Events/EventCard.js" 
+import styles from "../../EventsView/EventsView.module.css";
+import Description from "../../EventsView/EventDetails.js";
+import Results from "../../EventsView/Results.js";
+import Notification from "../../EventsView/Notification.js";
+import { useParams } from "react-router-dom";
+import { fetchOneEvent, submitQuery } from "../../../../api";
+import Query from "../../EventsView/Query.js";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
 const Profile = () => {
+  // const params = useParams();
   const currUser = useSelector((state) => state.auth.curruser);
-  useEffect(() => {
-    console.log("CurrUser", currUser);
-  }, [currUser]);
+  const [currentTab, setCurrentTab] = useState("Description");
+  // useEffect(() => {
+  //   console.log("CurrUser", currUser);
+  // }, [currUser]);
+
+  const tabs = {
+    Description: "Description" ,
+    Results: "Result" ,
+    Notification: "Notification" 
+  };
+  // const [currentTab, setCurrentTab] = useState("Description");
+  //const [event, setEvent] = useState();
+  // const toastStyle = {
+  //   position: "top-right",
+  //   autoClose: 2000,
+  //   hideProgressBar: true,
+  //   closeOnClick: true,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   progress: undefined,
+  //   theme: "dark",
+  // };
+  // useEffect(() => {
+  //   //console.log("USef Eeevt");
+  //   fetchOneEvent(setEvent, params.id)
+  //     .then((res) => {
+  //       //console.log(res);
+  //       setEvent(res);
+  //       // //console.log(event);
+  //     })
+  //     .catch((err) => {
+  //       //console.log(err);
+  //     });
+  // }, []);
+  // const handleQuery = (query) => {
+  //   if (!query.subject || !query.queryDesc) {
+  //     alert("All Fields are Mandatory");
+  //     return;
+  //   }
+  //   query.eventId = params.id;
+  //   submitQuery(currUser.token, query)
+  //     .then((res) => {
+  //       toast.info(res, toastStyle);
+  //       //console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       toast.error(err, toastStyle);
+  //       //console.log(err);
+  //     });
+  //   //console.log(query);
+  // };
 
   return (
     <div className="ud__profile">
-      <nav class="nav">
-      <ul class="nav-list">
-        <li class="nav-item">
-          <a class="active" href="#">Profile</a>
-        </li>
-        <li class="nav-item">
-          <a href="#">My Passes</a>
-        </li>
-        <li class="nav-item">
-          <a href="#">My Events</a>
-        </li>
-        <li class="nav-item">
-          <a href="#">Notification</a>
-        </li>
-      </ul>
-    </nav>
-    <div className="profile-container">
+      
+      
+      <nav className={styles.eventsnav}>
+            <ol>
+              {Object.keys(tabs).map((tab) => (
+                <li
+                  key={tab}
+                  className={currentTab == tab ? styles.active : ""}
+                  onClick={() => setCurrentTab(tab)}
+                >
+                  {tab}
+                </li>
+              ))}
+            </ol>
+            <hr/>
+          </nav>
+      <div className="profile-container">
+        <EventCard />
 
-       <div className="pass-container">
-        <div className="pass">
-          <img src="img/roboWars.png" alt="pass-image" class="pass-img" />
-        </div>
-        <div className="pass-id">
-          <p className="id-text">#1DF34</p>
-          <img src="img/qr.jpg" alt="id qr code" class="id-qr" />
-        </div>
-      </div> 
-      <div className="user-details">
-        <img src={prof} alt="" className="user-img" />
-        <h3 className="username">USER NAME</h3>
-        <div className="details">
-          <p>Registered email</p>
-          <div className="user-email">{currUser.profile.email}</div>
-          <p>Registered Number</p>
-          <div className="user-number">123456789</div>
-          <p className="change-no">Change Number?</p>
-          <div className="referral-id">Referal id: 1DF34</div>
+        <div className="user-details">
+          <img src={prof} alt="" className="user-img" />
+          <h3 className="username">USER NAME</h3>
+          <div className="details">
+              <p>Registered email</p>
+              <div className="user-email">{currUser.profile.email}</div>
+              <p>Registered Number</p>
+              <div className="user-number">123456789</div>
+              <p className="change-no">Change Number?</p>
+              <div className="referral-id">Referal id: 1DF34</div>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-
+    
   );
 };
 
--- src/screens/containers/Events/EventCard.js
+++ src/screens/containers/Events/EventCard.js
@@ -1,145 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
-function EventCard(props) {
-  const styles = [
-    {
-      backCol: "#FF4B4B",
-      leftCol: "white",
-      rightCol: "#FFC700",
-    },
-    {
-      backCol: "#2E2D2D",
-      leftCol: "white",
-      rightCol: "#FFC700",
-    },
-    {
-      backCol: "white",
-      leftCol: "#2D2D2D",
-      rightCol: "#FFC700",
-    },
-  ];
-  const { card, index } = props;
-  const curr = styles[index % 3];
-  const animDelay =
-    (index * 6).toString() + "0ms";
-  const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  //console.log("/events/" + card.category + "/" + card._id);
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let date = new Date(card.from);
+import roboWars from "../../../images/events/roboWars.png";
+import qr from "../../../images/events/qr_1.jpg";
+import "../../../styles/profile.css";
+function EventCard() {
   return (
-    <Link
-      to={
-        "/events/" +
-        card.category +
-        "/" +
-        card._id
-      }
-      style={{ "text-decoration": "none" }}
-    >
-      <div
-        className="eventE-card"
-        style={{ animationDelay: animDelay }}
-      >
-        <div
-          className="eventE-imgCon"
-          style={{ background: curr.backCol }}
-        >
-          <img src={card.imageUrl}></img>
-        </div>
-        <div
-          className="eventE-details"
-          style={{ background: curr.backCol }}
-        >
-          <div
-            className="eventE-left"
-            style={{ color: curr.leftCol }}
-          >
-            <div className="eventE-title">
-              {card.name}
-            </div>
-            {/* {card.amount != 0 && (
-              <div className="eventE-price">
-                <span style={{ fontSize: "0.7rem" }}>Price</span>
-                <br></br>
-                Rs.{card.amount}
-              </div>
-            )} */}
-            <button className="eventE-register">
-              Explore
-            </button>
-          </div>
-          <div
-            className="eventE-right"
-            style={{ color: curr.rightCol }}
-          >
-            <div className="eventE-sub">
-              <div className="eventE-sub1">
-                <span
-                  style={{ fontSize: "0.7rem" }}
-                >
-                  Date
-                </span>
-                <br></br>
-                <span
-                  style={{
-                    fontSize: "1.2rem",
-                    fontWeight: "800",
-                  }}
-                >
-                  Coming Soon !!
-                  {/* {weekday[date.getDay()]}
-                  <br></br>
-                  {date.getDate() +
-                    " " +
-                    monthNames[date.getMonth()] +
-                    " " +
-                    date.getFullYear()} */}
-                </span>
-              </div>
-
-              <div className="eventE-sub2">
-                <span
-                  style={{ fontSize: "0.7rem" }}
-                >
-                  Event Venue
-                </span>
-                <br></br>
-                Coming Soon !!
-                {/* <span style={{ fontSize: "1.2rem", fontWeight: "800" }}>
-                  {card.location}
-                </span>
-                <br></br>
-                <span style={{ fontSize: "1.1rem", fontWeight: "300" }}>
-                  {card.time}
-                </span> */}
-              </div>
-              <div className="eventE-sub1"></div>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div class="pass">
+        <img
+          src={roboWars}
+          alt="pass-image"
+          class="pass-img"
+        />
       </div>
-    </Link>
+      <div class="pass-id">
+        <p class="id-text">#1DF34</p>
+        <img
+          src={qr}
+          alt="id qr code"
+          class="id-qr"
+        />
+      </div>
+    </div>
   );
 }
 
